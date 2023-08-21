bf41edc2eef8677b74d2f76f21cae9d216c10dfd
barnettalex99 <alexgbarnett@gmail.com>
2023-08-21 12:29:22 -0400

    connect oracle db

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//set up DB
+
+const oracledb = require('oracledb');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -37,5 +41,42 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//connect to DB
+
+async function runApp()
+{
+  console.log('runApp running');
+ let connection;
+  try {
+  // Use the connection string copied from the cloud console
+  // and stored in connstring.txt file
+  console.log('connection trying')
+  connection = await oracledb.getConnection({ user: "admin", password: "LocalLibrary1", connectionString: "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.us-ashburn-1.oraclecloud.com))(connect_data=(service_name=gd62a8892716de6_librarydb_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))" });
+// Create a table
+  await connection.execute(`begin execute immediate 'drop table book'; exception when others then if sqlcode <> -942 then raise; end if; end;`);
+  await connection.execute(`create table book (title varchar(100), author varchar(100), hasRead varchar(1))`);
+// Insert some rows
+const sql = `INSERT INTO book (title, author, hasRead) VALUES ('East of Eden', 'John Steinbeck', 'Y')`;
+await connection.execute(sql);
+// connection.commit(); // uncomment to make data persistent
+
+// Now query the rows back
+const result = await connection.execute(`SELECT * FROM book`);
+console.dir(result.rows, { depth: null });
+} catch (err) {
+console.error(err);
+} finally {
+if (connection)
+  {
+    try {
+      await connection.close();
+    } catch (err) {
+    console.error(err);
+    }
+  }
+  }
+}
+runApp();
 
 module.exports = app;
